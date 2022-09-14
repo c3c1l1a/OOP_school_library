ec32e7f25479a6df57c37d6101e0213f073fd39b
c3c1l1a <cesswangumukima@gmail.com>
2022-09-15 01:23:32 +0800

    Add test for capitalize decorator

--- src/capitalize_decorator.rb
+++ src/capitalize_decorator.rb
@@ -1,6 +1,10 @@
 require_relative './base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
+  attr_accessor :nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
   def correct_name
     plain_name = @nameable.correct_name
     plain_name.capitalize
